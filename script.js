509f4287bb0141dd6237e91a95786633028383d7
Ahkillah Davis <ahkillahdavis@gmail.com>
2016-11-28 09:55:14 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+console.log('js sourced');
+
+var employee = [];
+
+var newEmployee = function (){
+var fName = document.getElementById("fname").value;
+var lName = document.getElementById('lname').value;
+var idNum = document.getElementById('idnum').value ;
+var jobTitle = document.getElementById('title').value;
+var annualSalary = document.getElementById('salary').value;
+console.log(fName);
+employee.push(" ");
+$('input').val('');
+
+$(document).ready(function(){
+    $("#btn").click(function(){
+        $("body").append(" input");
+    });
+return false;
+};
+
+var addEmployee = {
+    firstName: firstName,
+    lastName: lastName,
+    idNumber: idNumber,
+    jobTitle: jobTitle,
+    annualSalary: annualSalary
+}
+var text = " ";
+for(var i= 0;i < employee.length; i++);
+ text += employee[0] + " " + employee[1]+ " " + employee[2]+ " " + employee[3]+ " " + employee[4];
+ }
